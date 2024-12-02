dc8fb223569cbe681fd3392cf008a12bfca97450
DINESH DATTA MANNAM <134314223+kirito2789@users.noreply.github.com>
2024-12-02 17:51:44 +0530

    Update bgr2gray.py

--- Infosys Springboard 1/Image_processing/bgr2gray.py
+++ Infosys Springboard 1/Image_processing/bgr2gray.py
@@ -1,5 +1,5 @@
 import cv2
-image = cv2.imread('/home/maryam/Downloads/image2.jpg')
+image = cv2.imread('/home/Dinesh Datta/Downloads/image2.jpg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('/home/maryam/Downloads/image1.jpg', gray_image)
 cv2.imshow('Grayscale Image', gray_image)
