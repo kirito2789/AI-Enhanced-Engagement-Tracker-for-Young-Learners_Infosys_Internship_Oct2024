1dd38ae7c2609cf7e3552d34db8ff23d072fab20
DINESH DATTA MANNAM <134314223+kirito2789@users.noreply.github.com>
2024-12-02 17:52:17 +0530

    Update bgr2gray.py

--- Infosys Springboard 1/Image_processing/bgr2gray.py
+++ Infosys Springboard 1/Image_processing/bgr2gray.py
@@ -1,7 +1,7 @@
 import cv2
 image = cv2.imread('/home/Dinesh Datta/Downloads/image2.jpg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('/home/maryam/Downloads/image1.jpg', gray_image)
+cv2.imwrite('/home/Dinesh Datta/Downloads/image1.jpg', gray_image)
 cv2.imshow('Grayscale Image', gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
